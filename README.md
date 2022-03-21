258553a42c7ff74fc9ab31bfbfa2a350aba174c0
banlify <banlify@163.com>
2022-03-21 12:37:44 +0800

    docs: readme.md

--- README.md
+++ README.md
@@ -1,3 +1,79 @@
 # markdown-it-plugins
 
 [![NPM version](https://img.shields.io/npm/v/markdown-it-plugins?color=a1b858&label=)](https://www.npmjs.com/package/markdown-it-plugins)
+
+## Install
+
+```shell
+npm i markdown-it-plugins -D
+```
+
+## Usage
+```js
+import MarkdownIt from 'markdown-it'
+import { collectBlockCode } from 'markdown-it-plugin'
+
+const markdown = new Markdown({
+  // ...
+})
+  .use(collectBlockCode)
+```
+
+## Plugins
+
+### collectBlockCode
+To add foldable functionality to a code block, you can also give it a code block name
+
+```js
+import { collectBlockCode } from 'markdown-it-plugin'
+
+markdown.use(collectBlockCode, options)
+```
+
+#### Options
+
+```ts
+interface CollectCodeBlock {
+  /**
+   * Show the code block language.
+   * @type {boolean}
+   * @default true
+   */
+  lang?: boolean
+
+  /**
+   * Shows the copy code block content button.
+   * @type {boolean}
+   * @default false
+   */
+  copy?: boolean
+
+  /**
+   * Copy the content button text.
+   * @type {string}
+   * @default: 'Copy'
+   */
+  copyText?: string
+
+  /**
+   * The delimiter for the code block name.
+   * @type {string}
+   * @default ':'
+   */
+  separator?: string
+
+  /**
+   * Display code block names.
+   * @type {boolean}
+   * @default false
+   */
+  blockName?: boolean
+
+  /**
+   * Always expand the code block.
+   * @type {boolean}
+   * @default true
+   */
+  alwaysOpen?: boolean
+}
+```
--- plugins/collect-block-code.ts
+++ plugins/collect-block-code.ts
@@ -7,21 +7,58 @@ import type {
 } from '../types/index'
 
 export interface CollectCodeBlock {
+  /**
+   * Show the code block language.
+   * @type {boolean}
+   * @default true
+   */
   lang?: boolean
+
+  /**
+   * Shows the copy code block content button.
+   * @type {boolean}
+   * @default false
+   */
   copy?: boolean
+
+  /**
+   * Always expand the code block.
+   * @type {boolean}
+   * @default true
+   */
+  open?: boolean
+
+  /**
+   * Copy the content button text.
+   * @type {string}
+   * @default: 'Copy'
+   */
   copyText?: string
+
+  /**
+   * The delimiter for the code block name.
+   * @type {string}
+   * @default ':'
+   */
   separator?: string
+
+  /**
+   * Display code block names.
+   * @type {boolean}
+   * @default false
+   */
   blockName?: boolean
 }
 
 export function collectBlockCode(
   md: Markdown,
   {
-    blockName,
     lang = true,
     copy = false,
-    separator = ':',
+    open = true,
     copyText = 'Copy',
+    separator = ':',
+    blockName = false,
   }: CollectCodeBlock = {},
 ) {
   const fence = md.renderer.rules.fence!
@@ -30,9 +67,9 @@ export function collectBlockCode(
     idx: number,
     ...args: [options: MarkdownOptions, env: unknown, self: MarkdownRenderer]
   ): string => {
-    const [language, name] = tokens[idx].info.split(separator).map(s => s.trim())
+    const [language, name] = tokens[idx].info.split(separator).map((s: string) => s.trim())
 
-    let template = `<details class="collect-block-code"><summary class="collect-block-trigger"><div class="trigger-lt"><i class="block-code-icon"></i>${blockName ? `<span class="block-code-name">${name}</span>` : ''}</div>`
+    let template = `<details class="collect-block-code" ${open ? 'open' : ''}><summary class="collect-block-trigger"><div class="trigger-lt"><i class="block-code-icon"></i>${blockName ? `<span class="block-code-name">${name}</span>` : ''}</div>`
 
     if (copy || blockName)
       template += `<div class="trigger-rt">${lang ? `<span class="block-code-lang">${language}</span>` : ''}${copy ? `<button class="copy-code-btn">${copyText}</button>` : ''}</div>`
